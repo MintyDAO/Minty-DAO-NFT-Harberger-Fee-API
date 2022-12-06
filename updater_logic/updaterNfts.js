c462d81b6c1f49c8a931bbd3a775750bc68abaa7
RuslanMirov <ruslan777mirov@gmail.com>
2022-12-06 16:20:54 +0200

    Update

--- updater_logic/updaterNfts.js
+++ updater_logic/updaterNfts.js
@@ -4,7 +4,26 @@ const getEvent = require('../web3/getEvent')
 const web3Provider = require('../web3/web3Provider')
 const web3 = web3Provider()
 
-module.exports = async (BlockLatest) => {
+const config = require('../config')
+const abi = require('../abi')
+
+// module.exports = async (BlockLatest) => {
+const test = async(fromBlock, toBlock) => {
   // get all collections addresses
+  const factory = new web3.eth.Contract(abi.NFT_FACTORY_ABI, config.NFT_FACTORY)
+  const collections = await factory.methods.getAllCollections().call()
+
+  console.log(collections)
+
+  // (address, abi, fromBlock, toBlock, eventName, web3)
+
   // listen events for all addresses
+
+  for(let i = 0; i < collections.length; i++) {
+    data = await getEvent(collections[i], abi.NFT_COLLECTION_ABI, fromBlock, toBlock, 'allEvents', web3)
+
+    console.log(data)
+  }
 }
+
+test(25214353, 25219353)
