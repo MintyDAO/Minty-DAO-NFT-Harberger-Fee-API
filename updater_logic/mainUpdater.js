fb8253e8e4fff0b1d414eec7c6cc237da36d5e50
RuslanMirov <ruslan777mirov@gmail.com>
2022-12-07 18:34:41 +0200

    Update

--- updater_logic/mainUpdater.js
+++ updater_logic/mainUpdater.js
@@ -1,18 +1,23 @@
 const web3Provider = require('../web3/web3Provider')
 const web3 = web3Provider()
 const updaterCollections = require('./updaterCollections')
+const updaterNftsByEvents = require('./updaterNftsByEvents')
 
 let prevBlock = 0
 
 module.exports = () => {
-  console.log("Run http block listener")
+  console.log("Run updater by http block listener")
   let timerId = setInterval(async () => runUpdates(), 3000)
 }
 
+// search updates in each block
 async function runUpdates() {
   const block = await web3.eth.getBlockNumber()
   if(prevBlock !== block){
-    updaterCollections()
+    // update collections
+    await updaterCollections()
+    // update nfts
+    await updaterNftsByEvents(block-3, block)
     prevBlock = block
   }
 }
